3ce00d0aeb355ea11bee60f7b6d1a6074f642f29
jorger <arisnova@arisnova.com>
2017-09-05 13:37:57 +0200

    Modified   server.js

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var http = require('http');
-var server = http.createServer();
-function control(petic, resp) {
-resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
-resp.end();
+//var sys = require ('sys'),
+var url = require('url'),
+http = require('http'),
+qs = require('querystring');
+
+
+function checkArray(json)
+{
+    for(var i = 0; i < json.length; i++) {
+        var obj = json[i];
+
+        console.log(obj);
+    }
 }
-server.on('request', control);
-server.listen(8080);
+
+
+http.createServer(function (req, res)
+{
+    if(req.method=='POST')
+    {
+            var body='';
+            req.on('data', function (data)
+            {
+                body +=data;
+            });
+            req.on('end',function()
+            {
+                var POST =  JSON.parse(body);
+                console.log(POST);
+                res.end();
+            });
+    }
+    else if(req.method=='GET') {
+        var url_parts = url.parse(req.url,true);
+        console.log(url_parts.query);
+    }
+}).listen(8080, "172.30.188.66");
