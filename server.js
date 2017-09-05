7e08b1c7a405ddb24d4c5f6123842c60371ac599
jorger <arisnova@arisnova.com>
2017-09-05 22:04:37 +0200

    Modified   server.js

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 #!/bin/env node
-const express = require('express')  
-const app = express()
+//var sys = require ('sys'),
+var url = require('url'),
+http = require('http'),
+qs = require('querystring');
 
-//var server_port = process.env.NODE_SERVICE_PORT_8080_TCP || 8081
-//var server_ip_address = process.env.NODE_PORT_8080_TCP_ADDR || '127.0.0.1'
 
-//console.log("El puerto es : " + server_port);
-//console.log("La IP es :" + server_ip_address);
+function checkArray(json)
+{
+    for(var i = 0; i < json.length; i++) {
+        var obj = json[i];
 
-app.use((request, response, next) => {  
-  console.log(request.headers)
-  next()
-})
-
-app.use((request, response, next) => {  
-  request.chance = Math.random()
-  next()
-})
-
-app.get('/', (request, response) => {  
-  response.json({
-    chance: request.chance
-  })
-})
-
-app.listen(8080, '0.0.0.0', function () {
-  //console.log( "Listening on " + server_ip_address + ", port " + server_port )
-  console.log("Iniciando ...");
-});
-
-/*//
-for (var prop in process.env){
-	console.log(prop + ": " + process.env[prop])
+        console.log(obj);
+    }
 }
-*/
+
+
+http.createServer(function (req, res)
+{
+    if(req.method=='POST')
+    {
+            var body='';
+            req.on('data', function (data)
+            {
+                body +=data;
+            });
+            req.on('end',function()
+            {
+                var POST =  JSON.parse(body);
+                console.log(POST);
+                res.end();
+            });
+    }
+    else if(req.method=='GET') {
+        var url_parts = url.parse(req.url,true);
+        console.log(url_parts.query);
+    }
+}).listen(8080, "0.0.0.0");
