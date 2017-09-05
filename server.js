06218dac79b71be212f0de09be276237f426399a
jorger <arisnova@arisnova.com>
2017-09-05 14:45:09 +0200

    Modified   server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ qs = require('querystring');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
 
 function checkArray(json)
 {
@@ -41,4 +37,6 @@ http.createServer(function (req, res)
         var url_parts = url.parse(req.url,true);
         console.log(url_parts.query);
     }
-}).listen(8080,'127.0.0.1');
+}).listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
