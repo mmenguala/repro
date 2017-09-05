7194a31c0f3a3886958e4e26597f7cf0ed8ef276
jorger <arisnova@arisnova.com>
2017-09-05 13:27:16 +0200

    Modified   server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-// importar
-var express = require('express');
- 
-// instanciar
-var app = express();
- 
-// ruteo
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
-});
-app.get('/about', function(req, res){
-  res.sendfile(__dirname + '/public/about.html');
-});
-app.get('/hola.html', function(req, res){
-  res.sendfile(__dirname + '/hola.html');
-});
-// escuchar
-app.listen(8080);
- 
-console.log("Servidor Express escuchando en modo %s", app.settings.env);
+var http = require('http');
+var server = http.createServer();
+function control(petic, resp) {
+resp.writeHead(200, {'content-type': 'text/plain'});
+resp.write('Hola, Mundo!');
+resp.end();
+}
+server.on('request', control);
+server.listen(8080);
